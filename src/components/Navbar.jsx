31cbe958899a9956220f8005ed586726f566fa14
Sachin Gavali <sachingavali321@gmail.com>
2024-05-28 11:36:52 +0530

    set up ready

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <header className="w-full py-4 fixed top-4 left-0 shadow-lg bg-transparent flex items-center justify-around">
+      <div>
+        <h2> Sachin Gavali </h2>
+      </div>
+      <nav>
+<a href="#">Heyeyy</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
